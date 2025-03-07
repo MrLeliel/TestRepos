d2c9a13926c111c98fc1797f854fd76a507e8c60
MrLeliel <zs23013983@estudiantes.uv.mx>
2025-03-07 16:46:14 -0600

    Create EjPython.py
    Se muestra unos ejemplos de los comandos "while" y "for"

---
+++ EjPython.py
@@ -0,0 +1,14 @@
+#Ejmeplo "while"
+contador = 5
+
+while contador > 0:
+    print(f"Tiempo restante: {contador} segundos")
+    contador -= 1
+
+print("¡Tiempo terminado!")
+
+#Ejemplo "for"
+for num in range(2, 21, 2):  # Empezamos en 2, terminamos en 20, avanzamos de 2 en 2
+    print(num, end=" ")  # Imprime en una sola línea
+
+#Sánchez Hernández Christian 07/03/25
